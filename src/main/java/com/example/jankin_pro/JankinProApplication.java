9758a4524d3d54235f017811a3c2b949b8935de6
mukesh singh <kumarmukeshsapna2001@gmail.com>
2026-03-02 12:14:36 +0530

    first commit

---
+++ src/main/java/com/example/jankin_pro/JankinProApplication.java
@@ -0,0 +1,13 @@
+package com.example.jankin_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankinProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankinProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jankin_pro/JankinProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jankin_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankinProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
